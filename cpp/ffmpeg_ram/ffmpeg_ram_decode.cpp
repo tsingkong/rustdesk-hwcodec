f1524ae438ba7d07ffd56cdc27d9c18d525a5ee8
tsingkong <tsingkong@163.com>
2026-03-14 23:03:34 +0800

    fix AVFrame has no member named key_frame, FF_PROFILE_H264_HIGH & FF_PROFILE_HEVC_MAIN

--- cpp/ffmpeg_ram/ffmpeg_ram_decode.cpp
+++ cpp/ffmpeg_ram/ffmpeg_ram_decode.cpp
@@ -215,7 +215,7 @@ private:
 #if FF_API_FRAME_KEY
       int key_frame = frame_->flags & AV_FRAME_FLAG_KEY;
 #else
-      int key_frame = frame_->key_frame;
+      int key_frame = (frame_->pict_type == AV_PICTURE_TYPE_I) ? 1 : 0;
 #endif
 
       callback_(obj, tmp_frame->width, tmp_frame->height,
